1cee6c80d833635392b85d0321a97081082e037e
OutSquareCapital <166045166+OutSquareCapital@users.noreply.github.com>
2026-04-15 15:35:17 +0200

    added test case to check when/then aliases

--- tests/test_aliases.py
+++ tests/test_aliases.py
@@ -1,8 +1,9 @@
+import polars as pl
 from pyochain import Vec
 
 import pql
 
-_LF = pql.LazyFrame({"x": [1]})
+_LF = pql.LazyFrame({"x": [1], "y": ["hello"]})
 
 
 def _slct(*exprs: pql.Expr) -> Vec[str]:
@@ -17,3 +18,25 @@ def test_alias_mutability() -> None:
     assert _slct(original).first() == "x"
     assert _slct(prefixed).first() == "pre_x"
     assert _slct(aliased).first() == "renamed"
+
+
+def test_when_alias() -> None:
+    pql_x = pql.col("x")
+    pql_y = pql.col("y")
+    pl_x = pl.col("x")
+    pl_y = pl.col("y")
+    assert (
+        _slct(
+            pql.when(pql_x.gt(0)).then(pql_y).otherwise(pql_x),
+            pql.when(pql_x.gt(0)).then(pql.lit(1)).otherwise(pql_x),
+            pql.when(pql_x.gt(0)).then(pql_y.str.to_uppercase()).otherwise(pql_y),
+        ).into(list)
+        == _LF
+        .lazy()
+        .select(
+            pl.when(pl_x.gt(0)).then(pl_y).otherwise(pl_x),
+            pl.when(pl_x.gt(0)).then(pl.lit(1)).otherwise(pl_x),
+            pl.when(pl_x.gt(0)).then(pl_y.str.to_uppercase()).otherwise(pl_y),
+        )
+        .columns
+    )
